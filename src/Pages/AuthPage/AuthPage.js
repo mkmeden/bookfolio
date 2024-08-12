2670fc470c58882ee557efa592e4c1025d93205e
mkmeden <midhupayathuparambil@gmail.com>
2024-08-12 17:41:19 +0530

    search user fix

--- src/Pages/AuthPage/AuthPage.js
+++ src/Pages/AuthPage/AuthPage.js
@@ -11,16 +11,15 @@ import {
 } from "@chakra-ui/react";
 import { Divider } from "@chakra-ui/react";
 import Login from "../../Components/Auth/Login";
-import Signup from '../../Components/Auth/Signup'
-import useGAuth from '../../hooks/useGAuth'
+import Signup from "../../Components/Auth/Signup";
+import useGAuth from "../../hooks/useGAuth";
 import GAuth from "../../Components/Auth/GAuth";
 
 const AuthPage = () => {
+  const { GLogin } = useGAuth();
 
-  const {GLogin} = useGAuth();
-
-const [login , setLogin] = useState(true)
-    return (
+  const [login, setLogin] = useState(true);
+  return (
     <>
       <Flex
         backgroundSize="cover"
@@ -28,39 +27,62 @@ const [login , setLogin] = useState(true)
         backgroundImage="url('/grid.jpg')"
         w={"full"}
         h={"full"}
-        px={{ base : 10 ,md:20}}
+        px={{ base: 10, md: 20 }}
         direction={"column"}
         justifyContent={"center"}
       >
-        <Text   fontSize={{ base  : "5xl",  md : "7xl"}} color={"#2A74E2"} fontWeight={"bold"}>
+        <Text
+          fontSize={{ base: "5xl", md: "7xl" }}
+          color={"#2A74E2"}
+          fontWeight={"bold"}
+        >
           Welcome
         </Text>
 
-        { login? <Login /> : <Signup />}
+        {login ? <Login /> : <Signup />}
 
-        <Flex alignItems={"center"} gap={2} mt={10} >
+        <Flex alignItems={"center"} gap={2} mt={10}>
           <Divider borderWidth="1px" borderColor="gray.700" />
           <Text fontSize={"2xl"} color={"#2A74E2"} fontWeight={"bold"}>
             OR
           </Text>
-          <Divider borderWidth="1px" borderColor="gray.700"  />
+          <Divider borderWidth="1px" borderColor="gray.700" />
         </Flex>
 
-        <Flex justifyContent={"center"} gap={2} alignItems={'center'}>
-            <Text fontSize={"2xl"} color={"#2A74E2"} fontWeight={"bold"} >Sign up with</Text>
-             <Box w={"40px"} cursor={"pointer"} onClick={()=>{
-              
-              GLogin()}
-              
-              } >
-                <Image src ="/googleIcon.png"/>
-             </Box>
-        </Flex>
-        <Flex gap={2} justifyContent={"center"}  >
-            <Text fontSize={"2xl"} color={"#2A74E2"} fontWeight={"bold"} >{login?  "Dont have an account?":"Already have an account?"} </Text>
-            <Box cursor={"pointer"} onClick={()=> setLogin(!login)} >
-            <Text textDecoration="underline" fontSize={"2xl"} color={"#2A74E2"} fontWeight={"bold"}> {login? "Signup" : "Login"} </Text>
+        <Flex justifyContent={"center"} gap={2} alignItems={"center"}>
+          <Text fontSize={"2xl"} color={"#2A74E2"} fontWeight={"bold"}>
+            Sign up with
+          </Text>
+          <Box
+            w={"40px"}
+            cursor={"pointer"}
+            onClick={() => {
+              GLogin();
+            }}
+          >
+            <Box
+              _hover={{ transform: "scale(1.05)" }}
+              transition="transform 0.2s"
+            >
+              <Image src="/googleIcon.png" />
             </Box>
+          </Box>
+        </Flex>
+        <Flex gap={2} justifyContent={"center"}>
+          <Text fontSize={"2xl"} color={"#2A74E2"} fontWeight={"bold"}>
+            {login ? "Dont have an account?" : "Already have an account?"}{" "}
+          </Text>
+          <Box cursor={"pointer"} onClick={() => setLogin(!login)}>
+            <Text
+              textDecoration="underline"
+              fontSize={"2xl"}
+              color={"#2A74E2"}
+              fontWeight={"bold"}
+            >
+              {" "}
+              {login ? "Signup" : "Login"}{" "}
+            </Text>
+          </Box>
         </Flex>
       </Flex>
     </>
--- src/hooks/useGetUserByUsername.js
+++ src/hooks/useGetUserByUsername.js
@@ -1,34 +1,53 @@
 import React from "react";
 import useShowToast from "./useShowToast";
-import { collection, query, where,getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
 import { useState } from "react";
 
 const useGetUserByUsername = () => {
-
-    const showToast = useShowToast()
-    const [loading , setLoading] = useState(false);
-    const [user , setUser] = useState(null);
+  const showToast = useShowToast();
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
   const searchUser = async (username) => {
-
-    setLoading(true)
+    setLoading(true);
     try {
-      const q = query(collection(firestore, "users"), where("username", "==", username));
+      const q = query(
+        collection(firestore, "users"),
+        where("username", "==", username)
+      );
       const querySnapshot = await getDocs(q);
+
+      if (username.length === 0) {
+        showToast(
+          "Not found",
+          `No user with the name ${username} is found`,
+          "info"
+        );
+        setLoading(false);
+        return;
+      }
+
+      if (querySnapshot.empty) {
+        showToast(
+          "Not found",
+          `No user with the name ${username} is found`,
+          "info"
+        );
+        setLoading(false);
+        return;
+      }
+
       querySnapshot.forEach((doc) => {
         setUser(doc.data());
       });
-      
-
     } catch (error) {
-        showToast('Error', error.message , 'error')
-    }
-    finally{
-        setLoading(false)
+      showToast("Error", error.message, "error");
+    } finally {
+      setLoading(false);
     }
   };
 
-  return [searchUser,user,loading]
+  return [searchUser, user, loading];
 };
 
 export default useGetUserByUsername;
